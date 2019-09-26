7435ccb5addb0b489a36ba6e50711dbc4c66cfbc
GMuji <mujigmj777@gmail.com>
2019-09-26 10:39:23 +0800

    Add one python file

---
+++ calc numbers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 10:05:18 2019
+Parsing excel file
+@author: ASUS
+"""
+
+def add(x,y):
+    return x+y
+    
+def substract(x,y):
+    a=max(x,y)
+    return a-min(x,y)
+    
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+    
